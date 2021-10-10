33dc3f40d19d137788ea2b6483765213ee2b3c4d
danodic <danodic@outlook.com>
2021-10-10 05:25:00 -0300

    Adding a comment to pretend I care, while I just want to trigger the pipeline to test it. :dog:

--- src/main/java/dev/danodic/rushbeat/livenumbers/LiveNumber.java
+++ src/main/java/dev/danodic/rushbeat/livenumbers/LiveNumber.java
@@ -1,14 +1,27 @@
 package dev.danodic.rushbeat.livenumbers;
 
 /**
- *
- * @author danodic
+ * A live number is expected to update itself every time the update function is
+ * called. All live numbers are expected to represent itself as a double in
+ * its internal logic. Also, a live number is expected to finish updating at
+ * a given time.
  */
 public interface LiveNumber {
 
+    /**
+     * Updates this live number.
+     */
     public void update();
 
+    /**
+     * Returns the double representation of this live number.
+     * @return the double value of this live number.
+     */
     public double doubleValue();
 
+    /**
+     * Returns wheter the number is done updating.
+     * @return true if the number has finished updating.
+     */
     public boolean isDone();
 }
